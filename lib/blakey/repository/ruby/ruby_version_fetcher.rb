a828cb6b3eeeeabe183f00be06c06e99d2466db7
Calvin Hughes <calvinhughes@users.noreply.github.com>
2020-12-22 22:45:57 +0000

    Report only the version number without such ruby prefix for Ruby repositories (#3)
    This ensures that such instances of a version reported from the Gemfile like 'ruby 2.7.1' are returned just like '2.7.1'
    
    Resolves #1

--- lib/blakey/repository/ruby/ruby_version_fetcher.rb
+++ lib/blakey/repository/ruby/ruby_version_fetcher.rb
@@ -10,12 +10,20 @@ module Blakey
       end
 
       def version
-        gemfile_lock_parser.ruby_version || fetch_version_from_ruby_version_file
+        format_version(fetched_version)
       end
 
       private
 
       attr_reader :gemfile_lock_parser, :source
+
+      def format_version(version_string)
+        version_string.gsub(/^(.*?)(?=[0-9])/, '').strip
+      end
+
+      def fetched_version
+        gemfile_lock_parser.ruby_version || fetch_version_from_ruby_version_file
+      end
 
       def fetch_version_from_ruby_version_file
         source.read_file('.ruby-version')
